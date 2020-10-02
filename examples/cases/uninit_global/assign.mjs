f798686d5cf1e42f18a655969320dde698b8db82
MirandaWood <miranda.Wood@uk.ey.com>
2020-10-02 17:18:30 +0100

    feat: unint global now writes preimage to db file

--- examples/cases/uninit_global/assign.mjs
+++ examples/cases/uninit_global/assign.mjs
@@ -1,22 +1,22 @@
 import config from 'config';
 import utils from 'zkp-utils';
 import GN from 'general-number';
+import fs from 'fs';
 
 import { getContractInstance } from '../../../src/utils/contract.mjs';
 import { generateProof } from '../../../src/utils/zokrates.mjs';
-import { getSiblingPath } from '../../../src/utils/timber.mjs';
+import { getSiblingPath, getLeafIndex } from '../../../src/utils/timber.mjs';
 import logger from '../../../src/utils/logger.mjs';
 
 const { generalise } = GN;
-let currentSalt = generalise('69089008153250307177190178425993311722466992440572990705513631250807294133588'); // general number
-let currentValue = generalise('10');
-let currentCommitment = generalise('27634846343805787138311706540239887610761244549650326635140919565459517002');
-let root = generalise('5751622558927030719140143782094603438076539172391937980489652641072158835988'); // normally we would query this
+const db = '/app/examples/cases/uninit_global/db/preimage.json';
+let preimage;
+// normally we would query this
 // const contractAddress = '0x9b1f7F645351AF3631a656421eD2e40f2802E6c0'; // this too
-// let currentSalt;
-// let currentValue;
-// let currentCommitment;
-// let root;
+let currentSalt;
+let currentValue;
+let currentCommitment;
+let root;
 
 const formatProof = proof =>
   generalise(Object.values(proof).flat(Infinity))
@@ -26,7 +26,15 @@ const formatProof = proof =>
 export async function assign(_value) {
   const value = generalise(_value);
 
-  if (currentCommitment) {
+  if (fs.existsSync(db)) {
+    preimage = JSON.parse(
+      fs.readFileSync(db, 'utf-8', err => {
+        console.log(err);
+      }),
+    );
+    currentCommitment = generalise(preimage.commitment);
+    currentValue = generalise(preimage.value);
+    currentSalt = generalise(preimage.salt);
     logger.info(
       `Previous commitment: ${currentCommitment.integer} Preimage: ${currentValue.integer}, ${currentSalt.integer}`,
     );
@@ -45,7 +53,8 @@ export async function assign(_value) {
     ? await getSiblingPath('Assign', leafIndex, currentCommitment.integer)
     : new Array(33).fill(0); // will be ignored in circuit if no commitment exists
 
-  const index = generalise(path[31].node) || generalise(0);
+  let index = currentCommitment ? await getLeafIndex('Assign', currentCommitment.integer) : generalise(0);
+  index = generalise(index);
 
   root = generalise(path[0].value) || generalise(0);
   // root = generalise(root.hex(32, 31));
@@ -70,16 +79,16 @@ export async function assign(_value) {
     newCommitment.integer,
   ].flat(Infinity);
 
-  logger.info(allInputs);
+  logger.info(`Inputs to Zokrates: ${allInputs}`);
 
   // below assumes we have compiled the circuit w outputs in app/outputs/assign
   // idea: when compiling, we create the circuit in this dir, then call load-circuits to add them to the ./circuits folder zokrates_worker looks in
   const res = await generateProof(`assign`, allInputs);
   const proof = formatProof(res.proof);
 
-  logger.info(`proof: ${proof}`);
-  logger.info(`new commitment: ${newCommitment.integer}, ${newCommitment.hex(32)}`);
-  logger.info(`Preimage: ${value.integer}, ${newSalt.integer}`);
+  logger.info(`Proof: ${proof}`);
+  logger.info(`\nNew commitment: ${newCommitment.integer}, ${newCommitment.hex(32)}`);
+  logger.info(`\nPreimage: ${value.integer}, ${newSalt.integer}`);
   if (currentCommitment) {
     logger.info(`Nullfier: ${nullifier.integer}`);
   }
@@ -92,9 +101,13 @@ export async function assign(_value) {
       gas: config.web3.options.defaultGas,
     });
 
-  currentValue = value;
-  currentSalt = newSalt;
-  currentCommitment = newCommitment;
+  preimage = {
+    value: value.integer,
+    salt: newSalt.integer,
+    commitment: newCommitment.integer,
+  };
+
+  fs.writeFileSync(db, JSON.stringify(preimage, null, 4));
 
   return tx;
 }
--- test/test.mjs
+++ test/test.mjs
@@ -47,9 +47,10 @@ describe('Assign', async function () {
     it('should assign', async () => {
       try {
         await startEventFilter('Assign');
-        const tx = await assign(11);
+        const tx = await assign(5);
         // console.log(tx);
         leafIndex = tx.events.NewLeaf.returnValues[0];
+        console.log(`Merkle tree event returnValues:`);
         console.log(tx.events.NewLeaf.returnValues);
         await sleep(10);
       } catch (err) {
@@ -60,6 +61,7 @@ describe('Assign', async function () {
     it('should update the merkle tree', async () => {
       try {
         const path = await getSiblingPath('Assign', leafIndex);
+        console.log('Queried sibling path:');
         console.table(path, ['value', 'nodeIndex']);
       } catch (err) {
         logger.error(err);
@@ -68,12 +70,18 @@ describe('Assign', async function () {
     });
   });
   // eslint-disable-next-line func-names
-  // describe('Assign', async function () {
-  //   this.timeout(3660000);
-  //   it('should assign, nullifying the old commitment', async () => {
-  //     const tx = await assign(7);
-  //     console.log(tx);
-  //     leafIndex = tx.events.NewLeaf.returnValues.leafIndex;
-  //   });
-  // });
+  describe('Reassign', async function () {
+    this.timeout(3660000);
+    it('should assign, nullifying the old commitment', async () => {
+      try {
+        const tx = await assign(7);
+        // console.log(tx);
+        console.log(`Merkle tree event returnValues:`);
+        console.log(tx.events.NewLeaf.returnValues);
+      } catch (err) {
+        logger.error(err);
+        process.exit(1);
+      }
+    });
+  });
 });
