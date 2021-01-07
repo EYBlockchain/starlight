261ab38eb685710e3f0c9061cf2301d1c4bcd5ec
MirandaWood <miranda.Wood@uk.ey.com>
2021-01-07 17:47:38 +0000

    feat: redecorate now a traversal

--- src/redecorate.mjs
+++ src/redecorate.mjs
@@ -1,311 +1,43 @@
-/**
-Reads an input file line by line and passes each line for further processing
-*/
+/* eslint-disable no-param-reassign */
 
 import fs from 'fs';
+import NodePath from './traverse/NodePath.mjs';
 import logger from './utils/logger.mjs';
+import explode from './transformers/visitors/explode.mjs';
+import redecorateVisitor from './transformers/visitors/redecorateVisitor.mjs';
 
-const operators = ['+', '-', '*', '/', '%'];
-const eqOperators = [' = ', '+= ', '-= '];
+/**
+ * Inspired by the Transformer
+ * https://github.com/jamiebuilds/the-super-tiny-compiler
+ */
 
-function findNodeId(ast, line) {
-  const { type, name, rhs, newline } = line;
-  const { nodes } = ast.nodes[1];
-  let nodeId;
-  let op;
-  let eqop;
-  switch (type) {
-    case 'global':
-      for (const node of nodes) {
-        if (node.nodeType === 'VariableDeclaration' && node.name === `${name}`) {
-          nodeId = node.id;
-          break;
-        } else if (
-          node.nodeType === 'VariableDeclarationStatement' &&
-          node.declarations[0].name === `${name}`
-        ) {
-          nodeId = node.declarations[0].id;
-          break;
-        }
-      }
-      if (!nodeId) logger.debug(`Node Id for ${type} ${name} not found`);
-      break;
-    case 'assignment':
-      // TODO functionCalls, MemberAccess, IndexAccess
-      for (const operator of operators) {
-        if (rhs.includes(operator)) {
-          op = operator;
-          break;
-        }
-      }
-      for (const eqOperator of eqOperators) {
-        if (newline.includes(eqOperator)) {
-          eqop = eqOperator.trim();
-          break;
-        }
-      }
-      for (const node of nodes) {
-        if (node.nodeType === 'FunctionDefinition') {
-          const fnScope = { nodes: [{}, {}] };
-          fnScope.nodes[1].nodes = node.body.statements;
-          nodeId = findNodeId(fnScope, line);
-        }
-        if (nodeId) break;
-        if (
-          node.nodeType === 'ExpressionStatement' &&
-          node.expression.nodeType === 'Assignment' &&
-          node.expression.leftHandSide.name === name
-        ) {
-          // TODO change how this works so we can accurately match assignments - may get the wrong id here for anything but identifiers and literals
-          if (node.expression.rightHandSide.nodeType === 'Identifier') {
-            if (rhs.replace(';', '') !== node.expression.rightHandSide.name) {
-              continue;
-            } else if (node.expression.operator === eqop) {
-              nodeId = node.expression.leftHandSide.id;
-              break;
-            }
-            if (nodeId) break;
-          }
-          if (node.expression.rightHandSide.nodeType === 'Literal') {
-            if (!rhs.includes(node.expression.rightHandSide.value)) {
-              continue;
-            } else {
-              nodeId = node.expression.leftHandSide.id;
-              break;
-            }
-          }
-          if (node.expression.rightHandSide.nodeType === 'BinaryOperation' && !op) continue;
-          if (
-            node.expression.rightHandSide.nodeType === 'BinaryOperation' &&
-            op &&
-            node.expression.rightHandSide.operator.includes(op)
-          ) {
-            nodeId = node.expression.leftHandSide.id;
-            break;
-          }
-        } else if (
-          node.nodeType === 'ExpressionStatement' &&
-          node.expression.nodeType === 'Assignment' &&
-          node.expression.leftHandSide.nodeType === 'IndexAccess'
-        ) {
-          const key =
-            node.expression.leftHandSide.indexExpression.expression ||
-            node.expression.leftHandSide.indexExpression;
-          if (
-            name.includes(node.expression.leftHandSide.baseExpression.name) &&
-            name.includes(key.name)
-          ) {
-            if (node.expression.rightHandSide.nodeType === 'Identifier') {
-              if (rhs.replace(';', '') !== node.expression.rightHandSide.name) {
-                continue;
-              } else if (node.expression.operator === eqop) {
-                nodeId = node.expression.leftHandSide.id;
-                break;
-              }
-              if (nodeId) break;
-            } else if (node.expression.rightHandSide.nodeType === 'BinaryOperation' && !op) {
-              continue;
-            } else if (
-              node.expression.rightHandSide.nodeType === 'BinaryOperation' &&
-              op &&
-              node.expression.rightHandSide.operator.includes(op)
-            ) {
-              if (
-                rhs.includes(node.expression.rightHandSide.rightExpression.name) &&
-                rhs.includes(node.expression.rightHandSide.leftExpression.baseExpression.name) &&
-                rhs.includes(node.expression.rightHandSide.leftExpression.indexExpression.name)
-              ) {
-                nodeId = node.expression.leftHandSide.id;
-                break;
-              }
-              if (
-                node.expression.rightHandSide.leftExpression.baseExpression &&
-                rhs.includes(node.expression.rightHandSide.rightExpression.value) &&
-                rhs.includes(node.expression.rightHandSide.leftExpression.baseExpression.name) &&
-                rhs.includes(node.expression.rightHandSide.leftExpression.indexExpression.name)
-              ) {
-                nodeId = node.expression.leftHandSide.id;
-                break;
-              }
-            }
-          }
-        }
-      }
-      break;
-    case 'function':
-      for (const node of nodes) {
-        if (
-          node.nodeType === 'FunctionDefinition' &&
-          node.kind === 'function' &&
-          node.name === `${name}`
-        ) {
-          nodeId = node.id;
-          break;
-        }
-      }
-      if (!nodeId) logger.debug(`Node Id for ${type} ${name} not found`);
-      break;
-    case 'constructor':
-      for (const node of nodes) {
-        if (node.nodeType === 'FunctionDefinition' && node.kind === 'constructor') {
-          nodeId = node.id;
-          break;
-        }
-      }
-      if (!nodeId) logger.debug(`Node Id for ${type} ${name} not found`);
-      break;
-    case 'param':
-      for (const node of nodes) {
-        if (node.nodeType === 'FunctionDefinition') {
-          const fnScope = { nodes: [{}, {}] };
-          fnScope.nodes[1].nodes = node.parameters.parameters;
-          line.inFnParams = true;
-          nodeId = findNodeId(fnScope, line);
-        } else if (node.nodeType === 'VariableDeclaration') {
-          if (node.name === name && line.inFnParams) nodeId = node.id;
-        }
-        if (nodeId) break;
-        line.inFnParams = false;
-      }
-      break;
-    default:
-      logger.debug('This shouldnt happen');
-  }
-  return nodeId;
+function transformation1(oldAST, toRedecorate) {
+  const dummyParent = {
+    ast: oldAST,
+  };
+
+  const path = new NodePath({
+    parent: dummyParent,
+    key: 'ast', // since parent.ast = node
+    container: oldAST,
+    node: oldAST,
+  });
+
+  // We'll start by calling the traverser function with our ast and a visitor.
+  // The newAST will be mutated through this traversal process.
+  path.traverse(explode(redecorateVisitor), toRedecorate);
+  console.log(toRedecorate);
+  // At the end of our transformer function we'll return the new ast that we
+  // just created.
+  return path;
 }
 
-function addDecorators(ast, line) {
-  // separated out node id and sprinkling for now - will merge later
-  const newAST = ast;
-  const { nodes } = newAST.nodes[1];
-  switch (line.type) {
-    default:
-      for (const node of nodes) {
-        if (node.id === line.nodeId) {
-          switch (line.keyword) {
-            case 'secret':
-              node.isSecret = true;
-              break;
-            case 'unknown':
-              node.isUnknown = true;
-              break;
-            case 'known':
-              node.isKnown = true;
-              break;
-            default:
-          }
-          break;
-        }
-      }
-      break;
-    case 'assignment':
-      for (const node of nodes) {
-        // if (node.id === line.parentNodeId && line.keyword === 'secret') {
-        //   // TODO remove this error/ change it
-        //   logger.info(`Unnecessary assignment decorator for ${node.name}...`);
-        //   if (node.isSecret) {
-        //     logger.info(
-        //       `...but the declaration has correctly been decorated as secret, so that's ok`,
-        //     );
-        //     break;
-        //   } else {
-        //     throw new Error(`... the declaration for ${node.name} hasn't been decorated!`);
-        //   }
-        // }
-        if (node.nodeType === 'ExpressionStatement') {
-          if (node.expression.leftHandSide && node.expression.leftHandSide.id === line.nodeId) {
-            switch (line.keyword) {
-              case 'secret':
-                node.expression.leftHandSide.isSecret = true;
-                break;
-              case 'unknown':
-                node.expression.leftHandSide.isUnknown = true;
-                break;
-              case 'known':
-                node.expression.leftHandSide.isKnown = true;
-                break;
-              default:
-            }
-            break;
-          }
-        }
-        if (node.nodeType === 'FunctionDefinition') {
-          const fnScope = { nodes: [{}, {}] };
-          fnScope.nodes[1].nodes = node.body.statements;
-          addDecorators(fnScope, line);
-        }
-      }
-      break;
-    case 'param':
-      for (const node of nodes) {
-        if (node.nodeType === 'FunctionDefinition') {
-          const fnScope = { nodes: [{}, {}] };
-          fnScope.nodes[1].nodes = node.parameters.parameters;
-          addDecorators(fnScope, line);
-        } else if (node.nodeType === 'VariableDeclaration') {
-          if (node.id === line.nodeId) {
-            switch (line.keyword) {
-              case 'secret':
-                node.isSecret = true;
-                break;
-              case 'unknown':
-                node.isUnknown = true;
-                break;
-              case 'known':
-                node.isKnown = true;
-                break;
-              default:
-            }
-          }
-        }
-      }
-      break;
-    // default:
-    //   logger.info(`Not implemented`);
-  }
-  return ast;
-}
-
-function redecorate(solAST, toRedecorate, options) {
+// A transformer function which will accept an ast.
+export default function redecorate(ast, toRedecorate, options) {
   logger.info(`Creating decorated ast... `);
-
-  let newAST = solAST;
-
-  for (const line of toRedecorate) {
-    // find nodeId
-    switch (line.type) {
-      case 'assignment':
-        // TODO remove this error/ change it
-        if (line.keyword === 'secret') {
-          logger.info(`Warning: secret keyword used for assignment after declaration...`);
-          for (const check of toRedecorate) {
-            if (check.type === 'global' && check.name === line.name) {
-              logger.info(
-                `...but the declaration has correctly been marked as secret, so that's ok`,
-              );
-              line.parentNodeId = check.nodeId;
-              break;
-            } else if (check.type === line.type && check.name === line.name) {
-              throw new Error(
-                `...and the declaration hasn't been marked as secret, so the assignment can't be secret!`,
-              );
-            }
-          }
-        }
-        line.nodeId = findNodeId(solAST, line);
-        break;
-      default:
-        // same for function, constructor, and global
-        line.nodeId = findNodeId(solAST, line);
-    }
-    // decorate ast
-    newAST = addDecorators(newAST, line);
-  }
+  const newAST = transformation1(ast, toRedecorate);
 
   const zsolASTFilePath = `${options.parseDirPath}/${options.inputFileName}.zsol_ast.json`;
-  fs.writeFileSync(zsolASTFilePath, JSON.stringify(newAST, null, 4));
-
-  return newAST;
+  fs.writeFileSync(zsolASTFilePath, JSON.stringify(newAST.node, null, 4));
+  return JSON.parse(JSON.stringify(newAST.node, null, 4));
 }
-
-export default redecorate;
---
+++ src/transformers/visitors/redecorateVisitor.mjs
@@ -0,0 +1,339 @@
+/* eslint-disable no-param-reassign, no-shadow, no-unused-vars */
+
+import cloneDeep from 'lodash.clonedeep';
+import logger from '../../utils/logger.mjs';
+import { getNodeLocation, findReferencedDeclaration } from '../../types/solidity-types.mjs';
+import circuitTypes from '../../types/circuit-types.mjs';
+import { traverse, traverseNodesFast } from '../../traverse/traverse.mjs';
+
+export default {
+  SourceUnit: {
+    enter(path, state) {},
+
+    exit(path, state) {},
+  },
+
+  PragmaDirective: {
+    // TODO: We should probably check that the `.zsol` Pragma is 'supported'. The output Solidity's pragma will be limited to the latest-supported boilerplate code.
+    // However, for now, we'll just inherit the Pragma of the original and hope.
+    enter(path, state) {},
+    exit(path, state) {},
+  },
+
+  ContractDefinition: {
+    enter(path, state) {},
+
+    exit(path, state) {},
+  },
+
+  FunctionDefinition: {
+    enter(path, state) {
+      const { node } = path;
+      for (const line of state) {
+        switch (line.type) {
+          case 'function':
+            if (line.nodeId) break;
+            if (node.kind === 'function' && node.name === line.name) line.nodeId = node.id;
+            break;
+          case 'constructor':
+            if (line.nodeId) break;
+            if (node.kind === 'constructor' && node.name === line.name) line.nodeId = node.id;
+            break;
+          default:
+            break;
+          // line.nodeId = findNodeId(solAST, line);
+        }
+        if (line.nodeId === node.id) {
+          switch (line.keyword) {
+            default:
+              break;
+            case 'secret':
+              node.isSecret = true;
+              break;
+            // case 'known':
+            //   node.isKnown = true;
+            //   break;
+            // case 'unknown':
+            //   node.isUnknown = true;
+            //   break;
+          }
+        }
+      }
+    },
+
+    exit(path, state) {},
+  },
+
+  ParameterList: {
+    enter(path, state) {
+      const { node } = path;
+      for (const line of state) {
+        switch (line.type) {
+          case 'param':
+            if (line.nodeId) break;
+            for (const param of node.parameters) {
+              if (param.name === line.name) {
+                line.nodeId = param.id;
+                if (line.keyword === 'secret') {
+                  param.isSecret = true;
+                } else {
+                  throw new Error(`Parameters can't be marked as known/unknown`);
+                }
+              }
+            }
+            break;
+          default:
+            break;
+        }
+      }
+    },
+
+    exit(path) {},
+  },
+
+  Block: {
+    enter(path) {},
+
+    exit(path) {},
+  },
+
+  VariableDeclarationStatement: {
+    enter(path, state) {
+      const { node } = path;
+      for (const line of state) {
+        switch (line.type) {
+          case 'global':
+            if (line.nodeId) break;
+            if (node.declarations[0].name === line.name) line.nodeId = node.declarations[0].id;
+            break;
+          default:
+            break;
+        }
+        if (line.nodeId === node.declarations[0].id) {
+          switch (line.keyword) {
+            default:
+              break;
+            case 'secret':
+              node.declarations[0].isSecret = true;
+              break;
+            case 'known':
+              node.declarations[0].isKnown = true;
+              break;
+            case 'unknown':
+              node.declarations[0].isUnknown = true;
+              break;
+          }
+        }
+      }
+    },
+
+    exit(path) {},
+  },
+
+  BinaryOperation: {
+    enter(path) {},
+
+    exit(path) {},
+  },
+
+  Assignment: {
+    enter(path, state) {
+      const operators = ['+', '-', '*', '/', '%'];
+      const eqOperators = [' = ', '+= ', '-= '];
+      let op;
+      let eqop;
+      const { node } = path;
+      for (const line of state) {
+        switch (line.type) {
+          case 'assignment':
+            if (line.nodeId) break;
+            for (const operator of operators) {
+              if (line.rhs.includes(operator)) {
+                op = operator;
+                break;
+              }
+            }
+            for (const eqOperator of eqOperators) {
+              if (line.newline.includes(eqOperator)) {
+                eqop = eqOperator.trim();
+                break;
+              }
+            }
+            // we have an assignment we still need to redecorate:
+            if (line.name === node.leftHandSide.name) {
+              // we have an identifier with name = name of assigned secret var
+              // check: operator, rhs type
+              switch (node.rightHandSide.nodeType) {
+                case 'Identifier':
+                  if (line.rhs.replace(';', '') !== node.rightHandSide.name) {
+                    break;
+                  } else if (node.expression.operator === eqop) {
+                    line.nodeId = node.leftHandSide.id;
+                    break;
+                  }
+                  break;
+                case 'Literal':
+                  if (!line.rhs.includes(node.rightHandSide.value)) {
+                    break;
+                  } else {
+                    line.nodeId = node.leftHandSide.id;
+                    break;
+                  }
+                case 'BinaryOperation':
+                  if (!op) break;
+                  if (op && node.rightHandSide.operator.includes(op)) {
+                    line.nodeId = node.expression.leftHandSide.id;
+                    break;
+                  }
+                  break;
+                default:
+                  break;
+              }
+            } else if (node.leftHandSide.nodeType === 'IndexAccess') {
+              const key =
+                node.leftHandSide.indexExpression.expression || node.leftHandSide.indexExpression;
+              if (!line.name.includes(node.leftHandSide.baseExpression.name)) break;
+              if (!line.name.includes(key.name)) break;
+              // copied and pasted from above -- TODO not repeat code!
+              // ---
+              switch (node.rightHandSide.nodeType) {
+                case 'Identifier':
+                  if (line.rhs.replace(';', '') !== node.rightHandSide.name) {
+                    break;
+                  } else if (node.operator === eqop) {
+                    line.nodeId = node.leftHandSide.id;
+                    break;
+                  }
+                  break;
+                case 'Literal':
+                  if (!line.rhs.includes(node.rightHandSide.value)) {
+                    break;
+                  } else {
+                    line.nodeId = node.leftHandSide.id;
+                    break;
+                  }
+                case 'BinaryOperation':
+                  if (!op) break;
+                  if (op && node.rightHandSide.operator.includes(op)) {
+                    if (
+                      line.rhs.includes(node.rightHandSide.rightExpression.name) &&
+                      line.rhs.includes(node.rightHandSide.leftExpression.baseExpression.name) &&
+                      line.rhs.includes(node.rightHandSide.leftExpression.indexExpression.name)
+                    ) {
+                      line.nodeId = node.leftHandSide.id;
+                      break;
+                    }
+                    if (
+                      node.rightHandSide.leftExpression.baseExpression &&
+                      line.rhs.includes(node.rightHandSide.rightExpression.value) &&
+                      line.rhs.includes(node.rightHandSide.leftExpression.baseExpression.name) &&
+                      line.rhs.includes(node.rightHandSide.leftExpression.indexExpression.name)
+                    ) {
+                      line.nodeId = node.leftHandSide.id;
+                      break;
+                    }
+                    break;
+                  }
+                  break;
+                default:
+                  break;
+              }
+              // ----
+            }
+            // error checking:
+            if (line.nodeId === node.leftHandSide.id) {
+              switch (line.keyword) {
+                default:
+                  break;
+                case 'secret':
+                  logger.info(`Warning: secret keyword used for assignment after declaration...`);
+                  for (const check of state) {
+                    if (check.type === 'global' && check.name === line.name) {
+                      logger.info(
+                        `...but the declaration has correctly been marked as secret, so that's ok`,
+                      );
+                      line.parentNodeId = check.nodeId;
+                      break;
+                    } else if (check.type === line.type && check.name === line.name) {
+                      throw new Error(
+                        `...and the declaration hasn't been marked as secret, so the assignment can't be secret!`,
+                      );
+                    }
+                  }
+                  break;
+                case 'known':
+                  node.leftHandSide.isKnown = true;
+                  break;
+                case 'unknown':
+                  node.leftHandSide.isUnknown = true;
+                  break;
+              }
+            }
+
+            break;
+          default:
+            break;
+        }
+      }
+    },
+
+    exit(path, state) {},
+  },
+
+  ExpressionStatement: {
+    enter(path, state) {},
+
+    exit(node, parent) {},
+  },
+
+  VariableDeclaration: {
+    enter(path, state) {
+      const { node } = path;
+      for (const line of state) {
+        switch (line.type) {
+          case 'global':
+            if (line.nodeId) break;
+            if (node.name === line.name) line.nodeId = node.id;
+            break;
+          default:
+            break;
+        }
+        if (line.nodeId === node.id) {
+          switch (line.keyword) {
+            default:
+              break;
+            case 'secret':
+              node.isSecret = true;
+              break;
+            // case 'known':
+            //   node.isKnown = true;
+            //   break;
+            // case 'unknown':
+            //   node.isUnknown = true;
+            //   break;
+          }
+        }
+      }
+    },
+
+    exit(path) {},
+  },
+
+  ElementaryTypeName: {
+    enter(path) {},
+
+    exit(path) {},
+  },
+
+  Identifier: {
+    enter(path) {},
+
+    exit(path) {},
+  },
+
+  Literal: {
+    enter(path) {},
+
+    exit(path) {},
+  },
+};
