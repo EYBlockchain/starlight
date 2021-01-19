71af25c619cb53063311bf05de34c3d2a6735205
MirandaWood <miranda.Wood@uk.ey.com>
2021-01-19 13:17:07 +0000

    feat: changed owned -> isOwned, more comments

--- src/transformers/visitors/ownership/ownershipVisitor.mjs
+++ src/transformers/visitors/ownership/ownershipVisitor.mjs
@@ -65,12 +65,16 @@ export default {
 
     exit(path, state) {
       const { node, scope, parentPath } = path;
+
+      // not a nullification - then we dont care about this statement for ownerahip
       if (!node.expression.isNullification) return;
 
       const lhsNode = node.expression.leftHandSide || node.expression.subExpression;
       let lhsIdentifier;
       let lhsbinding;
       let isMapping;
+
+      // below - get lhs info
       if (lhsNode.nodeType === 'Identifier') {
         lhsIdentifier = lhsNode;
         lhsbinding = scope.getReferencedBinding(lhsIdentifier);
@@ -81,6 +85,7 @@ export default {
         const keyName = scope.getMappingKeyIndicator(lhsNode);
         lhsbinding = scope.getReferencedBinding(lhsIdentifier).mappingKey[keyName];
       }
+
       const lhsSecret = !!lhsbinding.isSecret;
       if (!lhsSecret) return;
 
@@ -89,6 +94,7 @@ export default {
       const ownerNode = scope.callerRestrictionNode;
       let blacklistedNode;
 
+      // look for any caller restriction in this function; if there is any, then this state can only be nullified by that owner
       switch (scope.callerRestriction) {
         case 'match':
           switch (ownerNode.nodeType) {
@@ -98,7 +104,7 @@ export default {
                   `Found two distinct owners of one state: ${lhsbinding.owner.name} and ${ownerNode.name}`,
                 );
               lhsbinding.owner = scope.getReferencedBinding(ownerNode);
-              lhsbinding.owned = true;
+              lhsbinding.isOwned = true;
               break;
             case 'Literal':
               if (lhsbinding.owner && lhsbinding.owner.value !== ownerNode.value)
@@ -106,26 +112,29 @@ export default {
                   `Found two distinct owners of one state: ${lhsbinding.owner.value} and ${ownerNode.value}`,
                 );
               lhsbinding.owner = ownerNode;
-              lhsbinding.owned = true;
+              lhsbinding.isOwned = true;
               break;
             default:
               break;
           }
           break;
         case 'notMatch':
+          // if we have msg.sender =/= someone, they are added to a notOwner indicator for later
           lhsbinding.notOwner = blacklistedNode;
           break;
         default:
-          // look for msg.sender
-          // 1st case - mapping w/ msg.sender key
+          // look for msg.sender (mapping w/ msg.sender key)
           if (!isMapping) break;
+          // if the mapping key node type isn't an address which is of type t_magic_message, we skip
           if (
             !lhsbinding.referencedKeyNodeType.includes('address') &&
             !lhsbinding.referencedKeyNodeType.includes('magic')
           )
             break;
+          // if the mapping key for THIS identifier is msg.sender...
           if (lhsbinding.referencedKeyisParam === 'msg') {
             let msgSenderEverywhere = true;
+            // we look through each nullifying path of this mapping[msg.sender]
             for (const nullPath of lhsbinding.nullifyingPaths) {
               // skip current node
               if (nullPath.node.id === node.id) continue;
@@ -139,14 +148,16 @@ export default {
               }
             }
             if (!msgSenderEverywhere) break;
+            // here: all nullifying paths have msg.sender as mapping key
+            // below error: if msgSenderEverywhere BUT we also have an admin restriction
             if (lhsbinding.owner && lhsbinding.owner.name !== 'msg')
               throw new Error(
                 `Found two distinct owners of one state: ${lhsbinding.owner.name} and msg.sender`,
               );
             lhsbinding.owner = lhsNode.indexExpression.expression;
-            lhsbinding.owned = true;
+            lhsbinding.isOwned = true;
             scope.getReferencedBinding(lhsIdentifier).owner = lhsNode.indexExpression.expression;
-            scope.getReferencedBinding(lhsIdentifier).owned = true;
+            scope.getReferencedBinding(lhsIdentifier).isOwned = true;
           }
           break;
       }
