d0354e8d4f25397332fa28367801971207a46039
iAmMichaelConnor <michael.connor@uk.ey.com>
2021-01-25 16:28:21 +0000

    test: can now check logger.warn warnings

--- test/prelim-traversals/index.mjs
+++ test/prelim-traversals/index.mjs
@@ -5,10 +5,11 @@ import pathjs from 'path';
 import chai from 'chai';
 import assert from 'assert';
 import { fileURLToPath } from 'url';
-import cloneDeep from 'lodash.clonedeep';
+
 import { readJsonFile, writeJsonFile } from '../helper/filing.mjs';
 
 import zappify from '../../src/index.mjs';
+import logger from '../../src/utils/logger.mjs';
 
 const { expect } = chai;
 const { AssertionError } = assert;
@@ -21,7 +22,7 @@ const args = process.argv;
 const testDataDir = pathjs.join(__dirname, 'test-data');
 const testDataFiles = fs.readdirSync(testDataDir).filter(file => file.includes('.zsol'));
 
-function mkDirs({ outputDirPath, parseDirPath }) {
+const mkDirs = ({ outputDirPath, parseDirPath }) => {
   try {
     fs.mkdirSync(outputDirPath, {
       recursive: true,
@@ -31,9 +32,9 @@ function mkDirs({ outputDirPath, parseDirPath }) {
   }
 
   if (!fs.existsSync(parseDirPath)) fs.mkdirSync(parseDirPath);
-}
+};
 
-function rmDir(dirPath) {
+const rmDir = dirPath => {
   try {
     fs.rmdirSync(dirPath, {
       recursive: true, // rm subdirectories too
@@ -42,33 +43,7 @@ function rmDir(dirPath) {
   } catch (err) {
     throw new Error(`Failed to remove the directory ${dirPath}`);
   }
-}
-
-/**
-
-FOR ASSERTING FAILURES (ERRORS) AND CHECKING THE ERROR TYPE IS CORRECT
-const assert = require('assert');
-const { AssertionError } = require('assert');
-
-const { functionundertest } = require('../index');
-
-describe('Testing Error States', () => {
-  it('Throws an error when called with missing arguments', () => {
-    try {
-      functionundertest(); // this should fail
-      assert.fail('expected exception not thrown'); // this throws an AssertionError
-    } catch (e) { // this catches all errors, those thrown by the function under test
-                  // and those thrown by assert.fail
-      if (e instanceof AssertionError) {
-        // bubble up the assertion error
-        throw e;
-      }
-      assert.equal(e.message, 'Invalid Arguments'); // Check error is as expected
-    }
-  });
-});
-
-*/
+};
 
 const collectScopesIntoArray = (path, state) => {
   const { scope } = path;
@@ -119,7 +94,7 @@ const formatScopesForTesting = scopes => {
 // TODO: remember to delete the temp dir for each zapp that these tests create, at the end of each loop!!!
 
 // We wrap the `it()` function in a closure, because `it()` is located within a 'for' loop. If we don't do this, mocha does weird async things.
-function itShouldCompareScopes(options, expected, actual) {
+function itShouldCompareOutputs(options, expected, actual, consoleWarnings) {
   const fileName = options.inputFileName;
   it(`${fileName}: indicators & bindings should be as expected`, () => {
     // try {
@@ -134,6 +109,7 @@ function itShouldCompareScopes(options, expected, actual) {
       scopes,
       errorType: null,
       errorMessage: null,
+      warningMessages: consoleWarnings,
     };
 
     if (args.includes('--json')) {
@@ -176,11 +152,53 @@ function itShouldThrowAnError(options, expected) {
   });
 }
 
+function itShouldWriteAnOutputFile(options, jsonFilePath, actual, consoleWarnings) {
+  const fileName = options.inputFileName;
+  it(`${fileName}: should write/overwrite '${fileName}.json'`, () => {
+    try {
+      const path = zappify(options);
+      const scopes = [];
+      path.traversePathsFast(collectScopesIntoArray, scopes);
+      formatScopesForTesting(scopes);
+      // `eql` tests for _deep_ object equality.
+      actual = {
+        scopes,
+        errorType: null,
+        errorMessage: null,
+        warningMessages: consoleWarnings,
+      };
+    } catch (err) {
+      actual = {
+        scopes: null,
+        errorType: err.name,
+        errorMessage: err.message,
+        warningMessages: null,
+      };
+    }
+    writeJsonFile(jsonFilePath, actual);
+    console.log(`Overwritten json file '${jsonFilePath}' with new expected values.`);
+
+    rmDir(options.outputDirPath); // clean up
+  });
+}
+
 describe('Test prelim traversals of .zsol files', function () {
   console.log(`Pass '--json' to see a JSON output of the scopes.`);
   console.log(
-    `Pass '--write' to write/overwrite the scopes to a JSON file. (But only do this if you know what you're doing!)\n`,
+    `Pass '--write <fileName>' to write/overwrite the scopes to a JSON file. (But only do this if you know what you're doing!)\n`,
   );
+
+  const originalWarn = logger.warn; // remember logger.warn; we'll be diverting it to a stub.
+  afterEach(function () {
+    logger.warn = originalWarn; // reset logger.warn to its correct functionality.
+  });
+
+  const consoleWarnings = [];
+  const stubWarn = warning => consoleWarnings.push(warning);
+  beforeEach(function () {
+    logger.warn = stubWarn; // temporarily divert logger.warn, so that we may collect warnings in memory, for testing.
+  });
+
   for (const zsolFile of testDataFiles) {
     const fileName = pathjs.basename(zsolFile, '.zsol');
     const zsolFilePath = pathjs.join(testDataDir, `${fileName}.zsol`);
@@ -195,43 +213,21 @@ describe('Test prelim traversals of .zsol files', function () {
       isTest: true,
       testType: 'prelim',
     };
-
     mkDirs(options);
 
-    let actual = {};
+    const actual = {};
 
     // If the tester (dev) trusts the output and wants to write it to file, to become the 'expected' object for future tests.
     // TODO: use yargs to enforce an argument be passed with '--write'
     if (args.includes('--write') && args[args.indexOf('--write') + 1] === fileName) {
-      try {
-        const path = zappify(options);
-        const scopes = [];
-        path.traversePathsFast(collectScopesIntoArray, scopes);
-        formatScopesForTesting(scopes);
-        // `eql` tests for _deep_ object equality.
-        actual = {
-          scopes,
-          errorType: null,
-          errorMessage: null,
-        };
-      } catch (err) {
-        actual = {
-          scopes: null,
-          errorType: err.name,
-          errorMessage: err.message,
-        };
-      }
-      writeJsonFile(jsonFilePath, actual); // JSON.stringify(actual, null, 2));
-      console.log(`Overwritten json file '${jsonFilePath}' with new expected values.`);
-
-      rmDir(options.outputDirPath); // clean up
+      itShouldWriteAnOutputFile(options, jsonFilePath, actual, consoleWarnings);
       continue;
     }
 
     /** Compare actual vs expected */
     const expected = readJsonFile(jsonFilePath);
     if (!expected.errorType) {
-      itShouldCompareScopes(options, expected, actual);
+      itShouldCompareOutputs(options, expected, actual, consoleWarnings);
     } else {
       itShouldThrowAnError(options, expected);
     }
