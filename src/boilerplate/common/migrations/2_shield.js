ec1fa45cc1807802e69862fc8ef1468fa1b4040e
MirandaWood <miranda.Wood@uk.ey.com>
2022-02-03 13:25:21 +0000

    feat: changed proof system to G16 for m1, fixed write vk issue

--- src/boilerplate/common/migrations/2_shield.js
+++ src/boilerplate/common/migrations/2_shield.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const BN256G2 = artifacts.require("BN256G2");
+const Pairing = artifacts.require("Pairing");
 const Verifier = artifacts.require("Verifier");
 CUSTOM_CONTRACT_IMPORT
 const CONTRACT_NAME = artifacts.require("CONTRACT_NAME");
@@ -11,14 +11,13 @@ functionNames.forEach(name => {
     fs.readFileSync(`/app/orchestration/common/db/${name}_vk.key`, 'utf-8'),
   );
   const vk = Object.values(vkJson).flat(Infinity);
-  vk.splice(-1, 1);
   vkInput.push(vk);
 });
 
 module.exports = deployer => {
   deployer.then(async () => {
-    await deployer.deploy(BN256G2);
-    await deployer.link(BN256G2, Verifier);
+    await deployer.deploy(Pairing);
+    await deployer.link(Pairing, Verifier);
     await deployer.deploy(Verifier);
     CUSTOM_CONTRACTS
     await deployer.deploy(CONTRACT_NAME, CUSTOM_INPUTS Verifier.address, vkInput);
