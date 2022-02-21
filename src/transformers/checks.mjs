8cf83d1d7bbfd512f79e14a950715859ab371383
MirandaWood <miranda.Wood@uk.ey.com>
2022-02-21 12:07:56 +0000

    chore: revert checks.mjs

--- src/transformers/checks.mjs
+++ src/transformers/checks.mjs
@@ -6,13 +6,13 @@ import logger from '../utils/logger.mjs';
 import explode from './visitors/explode.mjs';
 import unsupportedVisitor from './visitors/checks/unsupportedVisitor.mjs';
 import externalCallVisitor from './visitors/checks/externalCallVisitor.mjs';
-// import decoratorVisitor from './visitors/checks/decoratorVisitor.mjs';
-// import incrementedVisitor from './visitors/checks/incrementedVisitor.mjs';
-// import accessedVisitor from './visitors/checks/accessedVisitor.mjs';
-// import requireStatementVisitor from './visitors/checks/requireStatementVisitor.mjs';
-// import localDeclarationsVisitor from './visitors/checks/localDeclarationsVisitor.mjs';
-// import msgSenderParam from './visitors/checks/msgSenderParam.mjs';
-// import interactsWithSecretVisitor from './visitors/checks/interactsWithSecretVisitor.mjs';
+import decoratorVisitor from './visitors/checks/decoratorVisitor.mjs';
+import incrementedVisitor from './visitors/checks/incrementedVisitor.mjs';
+import accessedVisitor from './visitors/checks/accessedVisitor.mjs';
+import requireStatementVisitor from './visitors/checks/requireStatementVisitor.mjs';
+import localDeclarationsVisitor from './visitors/checks/localDeclarationsVisitor.mjs';
+import msgSenderParam from './visitors/checks/msgSenderParam.mjs';
+import interactsWithSecretVisitor from './visitors/checks/interactsWithSecretVisitor.mjs';
 
 /**
  * Inspired by the Transformer
@@ -50,21 +50,23 @@ function transformation1(oldAST) {
 
   // We'll start by calling the traverser function with our ast and a visitor.
   // The newAST will be mutated through this traversal process.
+  // Hari
   path.traverse(explode(externalCallVisitor), state);
   logger.verbose('No unsupported external calls');
-  // path.traverse(explode(decoratorVisitor), state);
-  // logger.verbose('No conflicting known/unknown decorators');
-  // path.traverse(explode(interactsWithSecretVisitor), state);
-  // logger.verbose('Secret interacts marked');
-  // path.traverse(explode(incrementedVisitor), state);
-  // logger.verbose('Incrementations marked');
-  // path.traverse(explode(accessedVisitor), state);
-  // logger.verbose('Accessed values marked');
-  // path.traverse(explode(requireStatementVisitor), state);
-  // logger.verbose('Require statements labelled public / private');
-  // path.traverse(explode(localDeclarationsVisitor), state);
-  // logger.verbose('Checked for unsupported local variable declarations');
-  // path.traverse(explode(msgSenderParam), state);
+  path.traverse(explode(decoratorVisitor), state);
+  logger.verbose('No conflicting known/unknown decorators');
+  path.traverse(explode(interactsWithSecretVisitor), state);
+  logger.verbose('Secret interacts marked');
+  path.traverse(explode(incrementedVisitor), state); // M
+  logger.verbose('Incrementations marked');
+  // Swati:
+  path.traverse(explode(accessedVisitor), state);
+  logger.verbose('Accessed values marked');
+  path.traverse(explode(requireStatementVisitor), state);
+  logger.verbose('Require statements labelled public / private');
+  path.traverse(explode(localDeclarationsVisitor), state);
+  logger.verbose('Checked for unsupported local variable declarations');
+  path.traverse(explode(msgSenderParam), state);
 
   // At the end of our transformer function we'll return the new ast that we
   // just created.
