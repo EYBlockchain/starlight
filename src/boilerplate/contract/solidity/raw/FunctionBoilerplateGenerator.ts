011c3b0e48003142e461390d454d021234dd89e4
SwatiEY <97442865+SwatiEY@users.noreply.github.com>
2022-04-29 12:36:11 +0100

    fix: Resolve Merge Conflicts

--- src/boilerplate/contract/solidity/raw/FunctionBoilerplateGenerator.ts
+++ src/boilerplate/contract/solidity/raw/FunctionBoilerplateGenerator.ts
@@ -42,9 +42,15 @@ class FunctionBoilerplateGenerator {
       return [
         ...(newNullifiers ? [`uint256[] ${visibility} newNullifiers`] : []),
         ...(commitmentRoot ? [`uint256 commitmentRoot`] : []),
+<<<<<<< HEAD
         ...(newCommitments ? [`uint256[] ${visibility} newCommitments`] : []),
         ...(checkNullifiers ? [`uint256[] ${visibility} checkNullifiers`] : []),
         ...(newCommitments || newNullifiers ? [`uint256[] ${visibility} proof`] : []),
+=======
+        ...(newCommitments ? [`uint256[] calldata newCommitments`] : []),
+        ...(checkNullifiers ? [`uint256[] calldata checkNullifiers`] : []),
+        ...(newCommitments || newNullifiers ? [`uint256[] calldata proof`] : []),
+>>>>>>> 72089e7 (feat: allow secret states in constructor for contract transpilation)
       ];
     },
 
