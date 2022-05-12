551dd27e5770d53c20d563cfc43850c6642f6e70
SwatiEY <97442865+SwatiEY@users.noreply.github.com>
2022-05-12 11:44:06 +0100

    Update src/codeGenerators/circuit/zokrates/toCircuit.ts
    Co-authored-by: Miranda Wood <miranda.wood@uk.ey.com>

--- src/codeGenerators/circuit/zokrates/toCircuit.ts
+++ src/codeGenerators/circuit/zokrates/toCircuit.ts
@@ -31,7 +31,7 @@ function codeGenerator(node: any) {
       const body = codeGenerator(node.body);
       let returnStatement : string[] = [];
       let returnName : string[] = [];
-      if(node.returnParameters.parameters) {
+      if(node.returnParameters) {
         node.parameters.parameters.forEach(param => {
           if(param.bpType === 'newCommitment')
           returnName.push(param.name);
