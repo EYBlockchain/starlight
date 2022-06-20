f2538929d1826a0a0ecf29ea3563d69c7940c92d
MirandaWood <miranda.Wood@uk.ey.com>
2022-06-20 11:08:23 +0100

    fix: fix merge issue for custom inputs + return statements

--- src/boilerplate/contract/solidity/nodes/FunctionBoilerplateGenerator.ts
+++ src/boilerplate/contract/solidity/nodes/FunctionBoilerplateGenerator.ts
@@ -104,9 +104,6 @@ class FunctionBoilerplateGenerator {
       const publicParams = params?.filter((p: any) => !p.isSecret).flatMap((p: any) => customInputsMap(p)).concat(customInputs);
 
       const functionName = path.getUniqueFunctionName();
-      if(path.node.returnParameters.parameters.length === 0){
-      publicParams?.push(1);
-      };
 
       const indicators = this.customFunction.getIndicators.bind(this)();
 
