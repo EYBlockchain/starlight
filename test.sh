7b9db9c49f2eb041c3e42e2babf6cb3630ec542c
Hari <harikrishnan.m@gds.ey.com>
2022-08-04 15:06:10 +0530

    local test for circuit and shieldcontract (#131)
    * local test for circuit and shieldcontract
    
    * fix : peform test only in zapps folder
    
    * fix: solc manual install

---
+++ test.sh
@@ -0,0 +1,23 @@
+npm i solc@^0.8.0 -g
+cd zapps/
+echo “$(tput setaf 3) SHIELDCONTRACTS” 
+SOLFILES=$(find . -type f -name "*Shield*.sol" -maxdepth 3 -mindepth 3)
+solarray=($SOLFILES)
+for solelement in "${solarray[@]}"
+do
+    DIR="$(dirname "${solelement}")"
+    solelement="${solelement:2}"
+    echo “$(tput setaf 7) $solelement compiling” 
+    solcjs --abi -o $DIR --include-path node_modules/ --base-path . $solelement || echo “$(tput setaf 1) $solelement failed”
+done
+
+echo “$(tput setaf 3) CIRCUITS” 
+ZOKFILES=$(find . -type f -name "*.zok" -maxdepth 3 -mindepth 3)
+zokarray=($ZOKFILES)
+for zokelement in "${zokarray[@]}"
+do
+    zokelement="${zokelement:1}"
+    echo “$(tput setaf 7) $zokelement compiling” 
+    docker run -v $PWD:/app/code --name testcircuits -ti ghcr.io/eyblockchain/zokrates-worker-m1 ./zokrates compile -i code$zokelement || echo “$(tput setaf 1) $zokelement failed”
+    docker rm testcircuits
+done
