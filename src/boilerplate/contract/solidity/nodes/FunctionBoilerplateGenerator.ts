7a3080f8defe33aa86acaf77f85377a6587c50ee
SwatiEY <97442865+SwatiEY@users.noreply.github.com>
2022-08-10 09:00:08 +0100

    fix: custom input bug fixed

--- src/boilerplate/contract/solidity/nodes/FunctionBoilerplateGenerator.ts
+++ src/boilerplate/contract/solidity/nodes/FunctionBoilerplateGenerator.ts
@@ -102,7 +102,7 @@ class FunctionBoilerplateGenerator {
       }
 
       const params = path.getFunctionParameters();
-      const publicParams = params?.filter((p: any) => (!p.isSecret && !p.interactsWithSecret)).concat(customInputs).map((p: any) => customInputsMap(p));
+      const publicParams = params?.filter((p: any) => (!p.isSecret && p.interactsWithSecret)).concat(customInputs).map((p: any) => customInputsMap(p));
       const publicParamstype = params?.filter((p: any) => (!p.isSecret)).map((p: any) => p.typeDescriptions.typeString);
       const functionName = path.getUniqueFunctionName();
 
