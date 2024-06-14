7da7ee64c14960e1ac3bf99396cce6aacb7a9191
Lydia Garms <Lydia.Garms@uk.ey.com>
2024-06-14 16:06:19 +0100

    fix: error in contract in Assign-Return

--- src/boilerplate/contract/solidity/raw/FunctionBoilerplateGenerator.ts
+++ src/boilerplate/contract/solidity/raw/FunctionBoilerplateGenerator.ts
@@ -87,7 +87,7 @@ class FunctionBoilerplateGenerator {
       });
     
       let msgSigCheck = ([...(isConstructor  ? [] : [`bytes4 sig = bytes4(keccak256("${functionName}(${parameter})")) ;  \n \t \t \t if (sig == msg.sig)`])]);
-      customInputs = customInputs?.flat(Infinity).filter(p => p.inCircuit);
+      customInputs = customInputs?.flat(Infinity).filter(p => (p.inCircuit || p.isReturn));
       
       const addCustomInputs = !customInputs  || (customInputs?.length == 1 && customInputs[0].name == '1') ? false : true;
       return [
