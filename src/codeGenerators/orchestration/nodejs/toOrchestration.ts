b556e7b236364f194018306455c82c2aac57b52b
Swati Rawal <swati.rawal@uk.ey.com>
2024-07-01 09:05:20 +0100

    chore: fixed return name

--- src/codeGenerators/orchestration/nodejs/toOrchestration.ts
+++ src/codeGenerators/orchestration/nodejs/toOrchestration.ts
@@ -51,6 +51,14 @@ export default function codeGenerator(node: any, options: any = {}): any {
        })
      }
         node.returnParameters = node.returnParameters.parameters.filter((paramnode: any) => (paramnode.isSecret || paramnode.typeName.name === 'bool')).map(paramnode => (paramnode.name)) || [];
+        node.returnParameters.forEach( (param, index) => {
+          if(decStates) {
+           if(decStates?.includes(param)){
+            node.returnParameters[index] = node.returnParameters[index]+'_change';
+          }
+        } else if(returnIsSecret[index])
+            node.returnParameters[index] = node.returnParameters[index];
+        })
         const fn = OrchestrationCodeBoilerPlate(node);
         const statements = codeGenerator(node.body);
         fn.statements.push(statements);
