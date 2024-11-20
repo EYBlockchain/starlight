b86b6112ad85590c044b1a87d1659feb1bbba8b9
kkahina <kahina.khacef@uk.ey.com>
2024-11-20 12:32:37 +0000

    Prettier-Orchestration

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,13 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginTs from "@typescript-eslint/eslint-plugin";
 import parser from "@typescript-eslint/parser";
-
+import eslintConfigPrettier from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
 
 
 export default [
+  eslintConfigPrettier,
+  
   {
   languageOptions: { 
    globals: globals.browser,
@@ -15,8 +18,10 @@ export default [
      sourceType: "module", 
    }, 
   }},
+  
    pluginJs.configs.recommended,
-  {
+
+   {
     rules: {
       "no-underscore-dangle": "off",
       "no-console": "warn",
@@ -25,16 +30,21 @@ export default [
       "no-restricted-syntax": "off",
       "no-restricted-globals": "off",
       "no-cond-assign": "off",
-      "no-undef": "warn",
+      "no-undef": "error",
       "semi": ["error", "always"], 
       "quotes": ["error", "double"], 
       "@typescript-eslint/no-unused-vars": ["warn"],
-      "prettier/prettier": "off"
+      indent: ["error", 2],
+      "prettier/prettier": "error",
+    
     },
   },
   {
     plugins: {
       "@typescript-eslint": pluginTs,
+      prettier: prettierPlugin,
+
+    
     },
   }
 ];
