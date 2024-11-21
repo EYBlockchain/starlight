f185d003a024c0ae390a9174118f9b8be4755f60
Lydia Garms <Lydia.Garms@uk.ey.com>
2024-11-21 13:36:17 +0000

    fix: error in variable declaration when mappings of structs are used with multiple mapping keys

--- src/transformers/visitors/toOrchestrationVisitor.ts
+++ src/transformers/visitors/toOrchestrationVisitor.ts
@@ -1511,13 +1511,11 @@ const visitor = {
           }
           i++;
         });      
-
         // check whether this should be a VariableDeclaration
         const firstEdit =
           (firstInstanceOfNewName && indicator.interactsWithSecret) ||
           (!indicator.isStruct && indicator.modifyingPaths[0]?.node.id === lhs?.id && indicator.isSecret && indicator.isWhole) ||
-          (indicator.isStruct && indicator instanceof MappingKey && indicator.container.modifyingPaths[0]?.node.id === lhs?.id && indicator.isSecret && indicator.isWhole);
-          
+          (indicator.isStruct && indicator instanceof MappingKey && indicator.modifyingPaths[0]?.node.id === lhs?.id && indicator.isSecret && indicator.isWhole);
         // We should only replace the _first_ assignment to this node. Let's look at the scope's modifiedBindings for any prior modifications to this binding:
         // if its secret and this is the first assigment, we add a vardec
         if (
@@ -1531,7 +1529,6 @@ const visitor = {
             )
               accessed = true;
           });
-
           // we still need to initialise accessed states if they were accessed _before_ this modification
           const accessedBeforeModification = indicator.isAccessed && indicator.accessedPaths[0].node.id < lhs.id && !indicator.accessedPaths[0].isModification();
 
