a1d898d144939a53b6fead98d7c428cb82d08f1b
Lydia Garms <Lydia.Garms@uk.ey.com>
2025-05-20 11:58:54 +0100

    expand testing for Assign and new testing for Arrays

--- apitest.js
+++ apitest.js
@@ -40,6 +40,19 @@ const callZAppAPIs = async (zappName , apiRequests, errorMessage) => {
 
 let res = {};
 
+const apiRequests_Arrays = [
+  { method: 'post', endpoint: '/add', data: { value: 10 } },
+  { method: 'post', endpoint: '/add', data: { value: 9 } },
+  { method: 'post', endpoint: '/remove', data: { value: 17 } },
+  { method: 'get', endpoint: '/getAllCommitments' },
+  { method: 'get', endpoint: '/getCommitmentsByVariableName', data: { name: 'a' } },
+  { method: 'get', endpoint: '/getCommitmentsByVariableName', data: { name: 'b', mappingKey: '0'} },
+  { method: 'get', endpoint: '/getCommitmentsByVariableName', data: { name: 'b', mappingKey: '1'} },
+  { method: 'get', endpoint: '/getCommitmentsByVariableName', data: { name: 'b', mappingKey: '2'} },
+];
+
+res.Arrays = await callZAppAPIs('Arrays', apiRequests_Arrays, 'Arrays Zapp failed');
+
 const apiRequests_Assign = [
   { method: 'post', endpoint: '/add', data: { value: 11 } },
   { method: 'post', endpoint: '/add', data: { value: 8 } },
@@ -71,6 +84,37 @@ const apiRequests_internalFunctionCallTest1 = [
 
 res.InternalFunctionCallTest1 = await callZAppAPIs('internalFunctionCallTest1', apiRequests_internalFunctionCallTest1, 'internalFunctionCallTest1 Zapp failed');
 
+describe('Arrays Zapp', () => {
+  it('tests APIs are working', async () => {
+    expect(res.Arrays[0].body.tx.event).to.equal('NewLeaves');
+    expect(res.Arrays[1].body.tx.event).to.equal('NewLeaves');
+    expect(res.Arrays[2].body.tx.event).to.equal('NewLeaves');
+  });
+  it('MinLeaf Index check', async () => {
+    expect(parseInt(res.Arrays[0].body.tx.returnValues.minLeafIndex)).to.equal(2);
+    expect(parseInt(res.Arrays[1].body.tx.returnValues.minLeafIndex)).to.equal(5);
+    expect(parseInt(res.Arrays[2].body.tx.returnValues.minLeafIndex)).to.equal(6);
+  });
+  it('Check number of commitments', async () => {
+    expect(res.Arrays[3].body.commitments.length).to.equal(7);
+  });
+  it('Check nullified commitments', async () => {
+    expect(res.Arrays[4].body.commitments[0].isNullified).to.equal(true);
+    expect(res.Arrays[4].body.commitments[1].isNullified).to.equal(true);
+    expect(res.Arrays[4].body.commitments[2].isNullified).to.equal(false);
+    expect(res.Arrays[5].body.commitments[0].isNullified).to.equal(false);
+    expect(res.Arrays[6].body.commitments[0].isNullified).to.equal(true);
+    expect(res.Arrays[6].body.commitments[1].isNullified).to.equal(false);
+    expect(res.Arrays[7].body.commitments[0].isNullified).to.equal(false);
+  });
+  it('Check value of final commitments', async () => {
+    expect(res.Arrays[4].body.commitments[2].preimage.value).to.equal(2);
+    expect(res.Arrays[5].body.commitments[0].preimage.value).to.equal(0);
+    expect(res.Arrays[6].body.commitments[1].preimage.value).to.equal(0);
+    expect(res.Arrays[7].body.commitments[0].preimage.value).to.equal(9);
+  });
+});
+
 describe('Assign Zapp', () => {
   it('tests APIs are working', async () => {
     expect(res.Assign[0].body.tx.event).to.equal('NewLeaves');
@@ -82,13 +126,16 @@ describe('Assign Zapp', () => {
     expect(parseInt(res.Assign[1].body.tx.returnValues.minLeafIndex)).to.equal(1);
     expect(parseInt(res.Assign[2].body.tx.returnValues.minLeafIndex)).to.equal(2);
   });
-  it('Check commitments', async () => {
+  it('Check number of commitments', async () => {
     expect(res.Assign[3].body.commitments.length).to.equal(3);
   });
   it('Check nullified commitments', async () => {
     expect(res.Assign[4].body.commitments[0].isNullified).to.equal(true);
     expect(res.Assign[4].body.commitments[1].isNullified).to.equal(true);
     expect(res.Assign[4].body.commitments[2].isNullified).to.equal(false);
+  });
+  it('Check value of final commitment', async () => {
+    expect(res.Assign[4].body.commitments[3].preimage.value).to.equal(3);
   });
 });
 
