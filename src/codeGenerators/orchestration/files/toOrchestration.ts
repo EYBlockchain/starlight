17b82544b2070101c394fb4330313ef73f8558cc
Lydia Garms <Lydia.Garms@uk.ey.com>
2025-05-21 14:59:58 +0100

    remove addConstructorNullifier function - this causes an error and isn't necesary anyway now that the smts have been removed for nullifiers

--- src/codeGenerators/orchestration/files/toOrchestration.ts
+++ src/codeGenerators/orchestration/files/toOrchestration.ts
@@ -152,7 +152,6 @@ const prepareIntegrationApiServices = (node: any) => {
   let fnboilerplate = fn.nodeType === 'IntegrationApiServiceFunction'?
   genericApiServiceFile.postStatements()[0]
     .replace(/CONTRACT_NAME/g, node.contractName)
-    .replace(/CONSTRUCTOR_INPUTS/g, node.functionNames.includes('cnstrctr') ? `await addConstructorNullifiers();` : ``)
     .replace(/FUNCTION_NAME/g, fn.name): genericApiServiceFile.postStatements()[1]
     .replace(/CONTRACT_NAME/g, node.contractName)
     .replace(/FUNCTION_NAME/g, fn.name) ;
